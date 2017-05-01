1c344280054eb05af8961d3c0816b9302dd3f02d
stephen paul <stephenpaul2727@gmail.com>
2017-04-30 23:56:09 -0400

    Slight modify

--- WebContent/angularCode.js
+++ WebContent/angularCode.js
@@ -12,3 +12,35 @@ navang.controller("navbarcontroller",function($scope){
 	
 });
 angular.bootstrap(document.getElementById("app2"),['profileApp']);
+
+
+
+
+
+
+
+
+
+
+//Initializing semaphore
+initialize(i)
+{
+    C->value = 1
+    return
+}
+
+A(Sem s)
+{
+    C->value--;
+    if(C>value < 0)
+	//block on semaphore
+    return
+}
+
+B(s)
+{
+    C->value++;
+    if(C->value <= 0)
+	//unblock one process or thread that is blocked on semaphore
+    return
+}
