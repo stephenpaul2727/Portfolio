3e09ae0583779fe804748d777055f1ea9abec25e
stephen paul <stephenpaul2727@gmail.com>
2017-05-01 00:25:59 -0400

    Final Updated code

--- WebContent/angularCode.js
+++ WebContent/angularCode.js
@@ -12,35 +12,3 @@ navang.controller("navbarcontroller",function($scope){
 	
 });
 angular.bootstrap(document.getElementById("app2"),['profileApp']);
-
-
-
-
-
-
-
-
-
-
-//Initializing semaphore
-initialize()
-{
-    C->value = 1;
-    return
-}
-
-A(Sem C)
-{
-    C->value--;
-    if(C>value < 0)
-	//block on semaphore
-    return
-}
-
-B(C)
-{
-    C->value++;
-    if(C->value <= 0)
-	//unblock one process or thread that is blocked on semaphore
-    return
-}
